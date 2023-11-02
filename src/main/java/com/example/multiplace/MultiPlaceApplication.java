a4fa1c7d244f17bbdd57cbc9dfbed5eaa1aa3744
VasilSV <114056331+VasilSV@users.noreply.github.com>
2023-11-02 10:34:38 +0200

    Initial commit

---
+++ src/main/java/com/example/multiplace/MultiPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.multiplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multiplace/MultiPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multiplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
