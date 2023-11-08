aa386e64cd96d97841ab7418ecf1b227a6180f81
VasilSV <114056331+VasilSV@users.noreply.github.com>
2023-11-08 14:37:46 +0200

    new

--- src/main/java/com/example/multiplace/service/UserEntityService.java
+++ src/main/java/com/example/multiplace/service/UserEntityService.java
@@ -1,9 +1,12 @@
 package com.example.multiplace.service;
 
+import com.example.multiplace.dtos.UserDTO;
 import com.example.multiplace.dtos.UserRegistrationDTO;
+import com.example.multiplace.dtos.UserRoleEntityDTO;
 import com.example.multiplace.model.entity.UserEntity;
 import com.example.multiplace.repository.UserRepository;
 import com.example.multiplace.repository.UserRoleRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,7 +14,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @Service
 public class UserEntityService {
@@ -19,14 +24,16 @@ public class UserEntityService {
     private final UserRoleRepository userRoleRepository;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
 
 
     public UserEntityService(UserDetailsService userDetailsService, UserRoleRepository userRoleRepository,
-                             UserRepository userRepository, PasswordEncoder passwordEncoder) {
+                             UserRepository userRepository, PasswordEncoder passwordEncoder, ModelMapper modelMapper) {
         this.userDetailsService = userDetailsService;
         this.userRoleRepository = userRoleRepository;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.modelMapper = modelMapper;
     }
 
     public void registerUser(UserRegistrationDTO registrationDTO,
@@ -49,6 +56,23 @@ public class UserEntityService {
         );
 
         successfulLoginProcessor.accept(authentication);
+    }
+    public List<UserDTO> getAllUsers() {
+        return
+                userRepository.findAll().stream()
+                        .map(UserEntityService::mapUserDTO)
+                        .collect(Collectors.toList());
+
+    }
+    private static UserDTO mapUserDTO (UserEntity userEntity){
+        UserRoleEntityDTO userRoleEntityDTO = new UserRoleEntityDTO()
+                .setRole(userEntity.getRoles().toString());
+        return new UserDTO()
+                .setId(userEntity.getId())
+                .setEmail(userEntity.getEmail())
+                .setUsername(userEntity.getUsername())
+                .setIdentificationNumber(userEntity.getIdentificationNumber())
+                .setRole(userRoleEntityDTO);
     }
 }
 
