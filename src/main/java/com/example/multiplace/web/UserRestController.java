d2fd42852e94208232276050e2389c6234827028
VasilSV <114056331+VasilSV@users.noreply.github.com>
2023-11-16 15:17:34 +0200

    new

--- src/main/java/com/example/multiplace/web/UserRestController.java
+++ src/main/java/com/example/multiplace/web/UserRestController.java
@@ -2,10 +2,12 @@ package com.example.multiplace.web;
 
 import com.example.multiplace.dtos.UserDTO;
 import com.example.multiplace.service.UserEntityService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 @CrossOrigin("*")
 @RestController
@@ -26,18 +28,25 @@ public class UserRestController {
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable("id") Long id) {
         Optional<UserDTO> userDTOOptional = userEntityService.findUserById(id);
-
+        if (!userDTOOptional.isPresent()){
+            throw new NoSuchElementException();
+        }
         return userDTOOptional.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
 
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<UserDTO> deleteUserById(@PathVariable("id")Long id){
+        System.out.println("neeeeee");
+        try {
+            userEntityService.deleteUserById(id);
+            return ResponseEntity.noContent().build();
 
-        userEntityService.deleteUserById(id);
-
-        return ResponseEntity.
-                noContent().
-                build();
+        } catch (Exception e) {
+            // Логика за обработка на грешките, например:
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
+
 }
