9f6ca97192ca3da821a9bf5c8562951abee133a7
VasilSV <114056331+VasilSV@users.noreply.github.com>
2024-03-20 13:19:43 +0200

    new up

--- src/main/resources/static/js/SumorderFunctions.js
+++ src/main/resources/static/js/SumorderFunctions.js
@@ -1,4 +1,4 @@
-// Функция за изчисляване и визуализация на общата сума
+
 function calculateTotalOrderPricesAndDisplay() {
     fetch('/api/orders/calculateTotalOrderPrices')
         .then(response => response.json())
@@ -9,7 +9,6 @@ function calculateTotalOrderPricesAndDisplay() {
         .catch(error => console.error('Грешка при извикване на заявка:', error));
 }
 
-// Функция за показване на общата сума
 function showTotalOrderPrice(totalOrderPrice) {
     var totalOrderPriceElement = document.getElementById('totalOrderPrices');
     totalOrderPriceElement.textContent = 'Обща цена на поръчката: ' + totalOrderPrice.toFixed(2) + ' лв.';
